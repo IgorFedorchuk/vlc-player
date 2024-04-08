7c51325e9a20ec5f8d20b918b7f3e6e4b50db9be
Igor <>
2024-04-08 19:43:48 +0200

    update links (+4 squashed commits) Squashed commits: [34e25b3] remove files [619c91a] rename folder [ff6ec54] add dependency [d2e7537] initial commit with fMIT player

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "vlc-player",
+    platforms: [.iOS(.v12)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "vlc-player",
+            targets: ["vlc-player"]
+        ),
+    ],
+    dependencies: [
+            .package(url: "https://github.com/tylerjonesio/vlckit-spm/", exact: "3.5.1")
+        ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "vlc-player", dependencies: [
+                .product(name: "VLCKitSPM", package: "vlckit-spm"),
+            ], resources: [.process("Resources")]
+        ),
+        .testTarget(
+            name: "Tests",
+            dependencies: ["vlc-player"]
+        ),
+    ]
+)
