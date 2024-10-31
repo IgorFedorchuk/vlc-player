221aeae7c975936d33421e2a93d41a5c426ea00c
Igor <>
2024-10-31 16:19:12 +0100

    clean func

--- Example/player/SceneDelegate.swift
+++ Example/player/SceneDelegate.swift
@@ -40,8 +40,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidEnterBackground(_: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
     }
 }
